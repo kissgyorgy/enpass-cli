02537f6ca2fd55f19d704ba6cf92325e5f02761b
Kiss György <kissgyorgy@me.com>
2020-01-08 16:57:21 +0100

    Make master password function when using a key file

--- enpass.py
+++ enpass.py
@@ -13,36 +13,41 @@ from Crypto.Cipher import AES
 PBKDF2_ROUNDS = 100_000
 ENPASS_DB = os.environ["ENPASS_DB"]
 KEY_FILE = os.environ["ENPASS_KEY_FILE"]
-MASTER_PASSWORD = os.environb[b"ENPASS_PASSWORD"]
+PASSWORD = os.environb[b"ENPASS_PASSWORD"]
 
-key_hex_xml = Path(KEY_FILE).read_bytes()
-print("KEY HEX XML:", key_hex_xml)
-key_hex = key_hex_xml[5:-6]
-print("KEY HEX:", key_hex)
-full_password = MASTER_PASSWORD + binascii.unhexlify(key_hex)
-print("FULL PASS", full_password)
 
-# The first 16 bytes of the database file are used as salt
-enpass_db_salt = open(ENPASS_DB, "rb").read(16)
+def make_master_password(password: bytes, key_path: Path):
+    key_hex_xml = Path(KEY_FILE).read_bytes()
+    # no need to use XML lib for such a simple string operation
+    cut_key_value = slice(5, -6)
+    key_hex = key_hex_xml[cut_key_value]
+    key_bytes = binascii.unhexlify(key_hex)
+    return password + key_bytes
 
-# The database key is derived from the master password
-# and the database salt with 100k iterations of PBKDF2-HMAC-SHA512
-enpass_db_key = hashlib.pbkdf2_hmac(
-    "sha512", full_password, enpass_db_salt, PBKDF2_ROUNDS
-)
 
-# The raw key for the sqlcipher database is given
-# by the first 64 characters of the hex-encoded key
-enpass_db_hex_key = enpass_db_key.hex()[:64]
-print("FINAL KEY:", enpass_db_hex_key)
+def main():
+    master_password = make_master_password(PASSWORD, KEY_FILE)
 
-conn = sqlite.connect(ENPASS_DB)
+    # The first 16 bytes of the database file are used as salt
+    enpass_db_salt = open(ENPASS_DB, "rb").read(16)
 
-c = conn.cursor()
-c.row_factory = sqlite.Row
-c.execute(f"PRAGMA key=\"x'{enpass_db_hex_key}'\";")
-c.execute("PRAGMA cipher_compatibility = 3;")
-c.execute("SELECT * FROM Identity;")
+    # The database key is derived from the master password
+    # and the database salt with 100k iterations of PBKDF2-HMAC-SHA512
+    enpass_db_key = hashlib.pbkdf2_hmac(
+        "sha512", master_password, enpass_db_salt, PBKDF2_ROUNDS
+    )
 
-identity = c.fetchone()
-print(identity["Info"].hex())
+    # The raw key for the sqlcipher database is given
+    # by the first 64 characters of the hex-encoded key
+    enpass_db_hex_key = enpass_db_key.hex()[:64]
+
+    conn = sqlite.connect(ENPASS_DB)
+
+    c = conn.cursor()
+    c.row_factory = sqlite.Row
+    c.execute(f"PRAGMA key=\"x'{enpass_db_hex_key}'\";")
+    c.execute("PRAGMA cipher_compatibility = 3;")
+    c.execute("SELECT * FROM Identity;")
+
+    identity = c.fetchone()
+    print(identity["Info"].hex())
